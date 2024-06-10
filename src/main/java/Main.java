628f9f8224e49bfbe1343c2b24036ee1a3bafe6e
Vlad Flore <flore.vlad@gmail.com>
2024-06-10 19:24:18 +0200

    pass 3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,36 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
+  private static final String NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n";
+  private static final String OK = "HTTP/1.1 200 OK\r\n\r\n";
+
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
-    ServerSocket serverSocket = null;
     Socket clientSocket = null;
-    
-    try {
-      serverSocket = new ServerSocket(4221);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
+
+    try (ServerSocket serverSocket = new ServerSocket(4221)) {
       serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
-
-      var status = "HTTP/1.1 200 OK\r\n\r\n";
-      clientSocket.getOutputStream().write(status.getBytes());
-
+      clientSocket = serverSocket.accept();
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+      String line = in.readLine();
+      if (line != null) {
+        String[] requestParts = line.split("\r\n");
+        String requestLine = requestParts[0];
+        String[] requestLineParts = requestLine.split("\s");
+        String requestTarget = requestLineParts[1];
+        if (requestTarget.equals("/")) {
+          out.write(OK);
+        } else {
+          out.write(NOT_FOUND);
+        }
+        out.flush();
+      }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
